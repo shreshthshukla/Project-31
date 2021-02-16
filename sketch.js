f944f22b2276dacf6640a541099e7669f738d036
shreshthshukla <74090495+shreshthshukla@users.noreply.github.com>
2021-02-16 19:48:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -20,9 +20,31 @@ function setup(){
          drops.push(new Drops(random(0,400),random(0,400)))
       }
    }
-}
+ }
 
 function draw(){
+   background("black");
    drops.display();
- }   
+
+   
+var rand = Math.round(random(1,4))
+console.log(rand)
+if(frameCount%80===0){
+   thunderCreatedFrame=frameCount;
+   thunder=createSprite(random(10,370),random(10,30),10,10)
+   switch(rand){
+      case 1: thunder.addImage(thunder1);
+      break;
+      case 2: thunder.addImage(thunder2);
+      break;
+      case 3: thunder.addImage(thunder3);
+      break;
+      case 4: thunder.addImage(thunder4);
+      default: break;   
+   }
+   thunder.scale=random(0.3,0.6)
+
+}
+
+}   
 
